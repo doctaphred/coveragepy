d3129d9ab6b3ea0d1cbe890a2925803febf4be0f
Frederick Wagner <frederick@frederickwagner.net>
2023-12-07 12:14:35 -0500

    WIP: See what full sys.path should normally be on Windows

--- tests/test_process.py
+++ tests/test_process.py
@@ -794,7 +794,7 @@ class EnvironmentTest(CoverageTest):
         actual = self.run_command("coverage run run_me.py")
         self.assert_tryexecfile_output(expected, actual)
 
-    def test_full_sys_path(self) -> None:
+    def test_full_sys_path_diff(self) -> None:
         """WIP: compare full path and argv, for Windows bug diagnosis."""
         self.make_file("run_me.py", """\
 import sys
@@ -807,6 +807,21 @@ print('sys.argv:', '\\n'.join(map(repr, sys.argv)))
         expected = self.run_command("python run_me.py")
         actual = self.run_command("coverage run run_me.py")
         self.assert_tryexecfile_output(expected, actual)
+
+    def test_full_sys_path_dump(self) -> None:
+        """WIP: compare full path and argv, for Windows bug diagnosis."""
+        self.make_file("run_me.py", """\
+import sys
+print('"DATA": "xyzzy"')
+print('sys.path:', '\\n'.join(map(repr, sys.path)))
+print()
+print('sys.argv:', '\\n'.join(map(repr, sys.argv)))
+""")
+        self.set_environ("PYTHONSAFEPATH", "1")
+        # expected = self.run_command("python run_me.py")
+        actual = self.run_command("coverage run run_me.py")
+        self.assert_tryexecfile_output("", actual)
+
 
     @pytest.mark.skipif(env.PYVERSION < (3, 11), reason="PYTHONSAFEPATH is new in 3.11")
     def test_pythonsafepath_dashm(self) -> None:
